4dda59979be575e483491b9efbc67510ad4757dc
Saket Narayan <saket@saket.me>
2017-05-30 20:12:51 +0530

    Remove unncessary code + fix formatting

--- src/com/sun/pdfview/PdfValidationResult.java
+++ src/com/sun/pdfview/PdfValidationResult.java
@@ -1,28 +1,28 @@
 package com.sun.pdfview;
 
 public enum PdfValidationResult {
-    /**
-     * Valid Pdf + Valid password (if it was password protected)
-     */
-    VALID,
+  /**
+   *  * Valid Pdf + Valid password (if it was password protected)
+   */
+  VALID,
 
-    /**
-     * The file is not a valid Pdf
-     */
-    INVALID_PDF,
+  /**
+   * The file is not a valid Pdf
+   */
+  INVALID_PDF,
 
-    /**
-     * The Pdf is password protected and the entered password is wrong.
-     */
-    INCORRECT_PASSWORD,
+  /**
+   * The Pdf is password protected and the entered password is wrong.
+   */
+  INCORRECT_PASSWORD,
 
-    /**
-     * Bastard uploaded a file that we just cannot understand.
-     */
-    UNKNOWN,
+  /**
+   * Bastard uploaded a file that we just cannot understand.
+   */
+  UNKNOWN,
 
-    /**
-     * Couldn't find the file provided
-     */
-    FILE_NOT_FOUND,
+  /**
+   * Couldn't find the file provided
+   */
+  FILE_NOT_FOUND,
 }
--- src/com/sun/pdfview/PdfValidator.java
+++ src/com/sun/pdfview/PdfValidator.java
@@ -11,79 +11,72 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 /**
- * Utility class for checking the validity of a Pdf file and / or its password.
+ * Utility class for checking the validity of a Pdf file and/or its password.
  */
 public class PdfValidator {
 
-    private static final String TAG = "Pdf";
+  /**
+   * Validate the PDF and check if it can be unlocked using <var>password</var>
+   * (if it's password protected).
+   *
+   * @return See {@link PdfValidationResult} for the possible return values.
+   * This will never be null.
+   */
+  public static PdfValidationResult validate(File pdf, String password) {
+    ByteBuffer byteBuffer;
+    try {
+      byteBuffer = FileUtils.readToByteBuffer(new FileInputStream(pdf));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return PdfValidationResult.FILE_NOT_FOUND;
+    }
+
+    try {
+      new PDFFile(byteBuffer, new PDFPassword(password));
+      return PdfValidationResult.VALID;
+
+    } catch (PDFAuthenticationFailureException authException) {
+      return PdfValidationResult.INCORRECT_PASSWORD;
+
+    } catch (InvalidPdfException invalidPdfException) {
+      return PdfValidationResult.INVALID_PDF;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return PdfValidationResult.UNKNOWN;
+    }
+  }
+
+  /**
+   * Check if a PDF is password protected. Prefer using {@link #validate(File, String)
+   * validate()} directly if you plan to call it immediately after calling this method
+   * (isPasswordProtected()) because it is an expensive call.
+   */
+  public static boolean isPasswordProtected(File file) {
+    return validate(file, null) == PdfValidationResult.INCORRECT_PASSWORD;
+  }
+
+  static class FileUtils {
+    private static final int BUFFER_SIZE = 0x20000; // ~130K.
 
     /**
-     * Validates the Pdf file and checks if it cna be unlocked using <var>password</var>
-     * (if it's password protected).
+     * Load a file to a Document.
      *
-     * @return See {@link PdfValidationResult}
+     * @return Document
+     * @throws IOException on IO error
      */
-    public static PdfValidationResult validate(File pdfFile, String password) {
-
-        ByteBuffer byteBuffer;
-        try {
-            byteBuffer = FileUtils.readToByteBuffer(new FileInputStream(pdfFile));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return PdfValidationResult.FILE_NOT_FOUND;
+    static ByteBuffer readToByteBuffer(InputStream inStream) throws IOException {
+      byte[] buffer = new byte[BUFFER_SIZE];
+      ByteArrayOutputStream outStream = new ByteArrayOutputStream(BUFFER_SIZE);
+      int read;
+      while (true) {
+        read = inStream.read(buffer);
+        if (read == -1) {
+          break;
         }
-
-        try {
-            new PDFFile(byteBuffer, new PDFPassword(password));
-            return PdfValidationResult.VALID;
-
-        } catch (PDFAuthenticationFailureException authException) {
-            return PdfValidationResult.INCORRECT_PASSWORD;
-
-        } catch (InvalidPdfException invalidPdfException) {
-            return PdfValidationResult.INVALID_PDF;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return PdfValidationResult.UNKNOWN;
-        }
-
+        outStream.write(buffer, 0, read);
+      }
+      return ByteBuffer.wrap(outStream.toByteArray());
     }
-
-    /**
-     * Checks if a Pdf is password protected. Prefer using {@link #validate(File, String)
-     * validate()} directly if you plan to call it immediately after calling this method
-     * (isPasswordProtected()) as it is an expensive method.
-     */
-    public static boolean isPasswordProtected(File file) {
-        return validate(file, null) == PdfValidationResult.INCORRECT_PASSWORD;
-    }
-
-    static class FileUtils {
-
-        private static final int bufferSize = 0x20000; // ~130K.
-
-        /**
-         * Loads a file to a Document.
-         *
-         * @return Document
-         * @throws IOException on IO error
-         */
-        static ByteBuffer readToByteBuffer(InputStream inStream) throws IOException {
-            byte[] buffer = new byte[bufferSize];
-            ByteArrayOutputStream outStream = new ByteArrayOutputStream(bufferSize);
-            int read;
-            while (true) {
-                read = inStream.read(buffer);
-                if (read == -1)
-                    break;
-                outStream.write(buffer, 0, read);
-            }
-            return ByteBuffer.wrap(outStream.toByteArray());
-        }
-
-
-    }
-
+  }
 }
